cf0c9a7ae68b2f39f1f613706d99187a9cfd467c
MrMoon125 <mr125moon@gmail.com>
2026-02-24 21:06:06 +0000

    upload full project

--- main.py
+++ main.py
@@ -401,5 +401,5 @@ def profile():
         return redirect(url_for('profile'))
     return render_template('profile.html', user=user)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
